65d67cbe54765bae422722b17aed68563952e92f
Ravindra Kaluram Dhore <r.dhore@easternenterprise.com>
2021-11-27 17:20:58 +0530

    2 way Data Binding

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun',
-        job: 'Ninja',
-        website: 'https://w3resource.com/',
-        websiteTag: '<a href="https://w3resource.com/">W3 Sources</a>'
+        name: '',
     },
     methods: {
-        greet: function(time) {
-            this.name = 'Ravi'
-            return 'Good' + ' ' + time + ' ' + this.name
-        }
+       
     }
 });
