a17c244cea9c47d83c2c87bb0f997058b2e46518
Ravindra Kaluram Dhore <r.dhore@easternenterprise.com>
2021-11-27 17:42:15 +0530

    Loopig with v-for

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        names: ['Ravi', 'Parth'],
+        ninjas: [
+            { name: 'Ravi', age: 25 },
+            { name: 'Parth', age: 35 },
+        ]
+    },
+});
